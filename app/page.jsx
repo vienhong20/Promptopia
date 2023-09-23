caf1415c84ea018aa09a41d9088b290cc2bbcc45
VIEN HONG <vienhong88@gmail.com>
2023-09-22 22:55:09 -0400

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -5,14 +5,14 @@ const Home = () => {
         <h1 className="head_text text-center">
             Discover & Share
             <br className="max-md:hidden" />
-            <span className="orange-gradient text-center"> AI-Powered Prompts</span>
+            <span className="orange_gradient text-center"> AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
             Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
         </p>
 
         {/*Feed*/}
-        
+
     </section>
   )
 }
