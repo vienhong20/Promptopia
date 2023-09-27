d055d012a0bf925d509b908c20cd7902adfed51c
VIEN HONG <vienhong88@gmail.com>
2023-09-27 12:22:46 -0400

    Update

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
     email: {
-        type:string,
+        type: String,
         unique: [true, 'Email already exists!'],
         required: [true, 'Email is required!'],
     },
