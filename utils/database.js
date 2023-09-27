5cdc8052c7a3fd59d9b1d5d0ce3600929b37b059
VIEN HONG <vienhong88@gmail.com>
2023-09-27 11:32:24 -0400

    Update

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,26 @@
 import mongoose from 'mongoose';
 
-let isConnect = false; //track the connection
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
-    if(isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-    
-    try {
-        await mongoose.connect(process.env.MONGODB_URI,{
-            dbName: 'share_prompt',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-        isConnect = true;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-        console.log('MongoDB connected')
-    } catch (error) {
-        console.log(error);
-    }
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
