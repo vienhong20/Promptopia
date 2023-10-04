361498995b774d475c18ab28af48435be595b8b3
VIEN HONG <vienhong88@gmail.com>
2023-10-04 19:16:57 -0400

    Update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard';
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+    
+  }
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text"
+          placeholder="search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
   )
 }
 
