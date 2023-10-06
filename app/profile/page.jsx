acd0b00ff5b0a99bad3ef7ab405a9c185dd32e0a
VIEN HONG <vienhong88@gmail.com>
2023-10-05 23:25:23 -0400

    Update

---
+++ app/profile/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState, useEffect} from 'react';
+import { useSession} from 'next-auth/react';
+
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
