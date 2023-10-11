3f5f465a3b81a51e256d1d6e608ce6b5ead7d077
VIEN HONG <vienhong88@gmail.com>
2023-10-10 23:45:00 -0400

    Update

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
-  
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -64,15 +64,17 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           {post.tag}
       </p> 
 
-      {session?.user.id === post.creator.id && pathName === '/profile' && (
-        <div>
-          <p className="font-inter text-sm 
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm 
             green-gradient cursor-pointer"
             onClick={handleEdit}
           >
             Edit
           </p>
-          <p className="font-inter text-sm 
+          <p 
+            className="font-inter text-sm 
             orange-gradient cursor-pointer"
             onClick={handleDelete}
           >
