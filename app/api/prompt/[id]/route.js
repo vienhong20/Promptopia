780f83564db44af6e98f43bf38625dd3eb88302f
VIEN HONG <vienhong88@gmail.com>
2023-10-11 23:48:45 -0400

    Update

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -36,3 +36,14 @@ export const PATH = async (request, { params }) => {
     }
 }
 //DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+        return new Response("Prompt deleted successfully", { status: 200 })
+
+    } catch (error) {
+        return new Response("Failed to delete prompt", { status: 500 })
+    }
+}
