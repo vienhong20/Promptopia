c41cbe83bb4090531044d983aec93ca76d9c79a0
VIEN HONG <vienhong88@gmail.com>
2023-10-11 23:43:33 -0400

    Update

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -18,5 +18,21 @@ export const GET = async (request, { params }) => {
 //PATH (update)
 export const PATH = async (request, { params }) => { 
     const { prompt, tag } = await request.json();
+
+    try { 
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if(!existingPrompt) return new Response("Prompt not found", { status: 404 })
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+        return new Response(JSON.stringify(existingPrompt), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to update prompt", { status: 500 })
+    }
 }
 //DELETE (delete)
